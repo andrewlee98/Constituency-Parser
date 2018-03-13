992761164b0cf77f6f855778f6c8f05aa023a021
Andrew Lee <andrew.warren.lee@gmail.com>
2018-03-12 21:27:59 -0400

    collected tree string list from file

--- src/gen.py
+++ src/gen.py
@@ -1,10 +1,54 @@
 import os
 
-treepath = "../treebank/treebank_3/parsed/prd/atis/"
-outpath = "../outputs/"
+class Node:
+    def __init__(self, label):
+        self.label = label
+        self.l = None
+        self.r = None
+
+treepath = "../treebank/treebank_3/parsed/mrg/atis/"
+outpath = "../data/"
+
+labels = set()
+text_list = []
+buffer = ""
 
 for filename in os.listdir(treepath):
     with open(treepath + filename, 'r') as f:
-        for line in f:
-            for word in line.split():
-                print(word)
+        text = f.read().replace('\n', '')
+
+text_list = text.split("( END_OF_TEXT_UNIT )")
+tree_text_list = []
+for t in text_list:
+    if "@" not in t and len(t) != 0:
+        tree_text_list.append(t)
+
+for t in tree_text_list:
+    for w in t.split():
+        if w[0] == "(" and len(w) > 1:
+            labels.add(w.strip("("))
+
+
+#
+# def rec_parse(tree_str):
+#     root = Node(string[0])
+#     # remove surrounding parentheses
+#
+#     # unary case
+#     if word
+#
+#     # search for right child
+#     s = tree_st[tree_st.find("(")+1:tree_st.rfind(")")]
+#     root.l = rec_parse(s)
+#
+#     # search for left child
+#     for word in tree_str:
+#         root.r = rec_parse(s)
+#
+#     return root
+
+for t in tree_text_list:
+    print(t)
+    print("**********")
+
+print(labels)
