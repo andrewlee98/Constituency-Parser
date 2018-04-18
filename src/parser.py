4360ca327b306f6fbe5d2204cf857c89972fa0da
Andrew Lee <andrew.warren.lee@gmail.com>
2018-04-18 10:23:34 -0400

    loaded vocab into network

--- src/parser.py
+++ src/parser.py
@@ -8,8 +8,11 @@ if __name__ == '__main__':
     pe = 50
     hidden = 200
     minibatch = 1000
+    epochs = 5
     net_properties = NetProperties(we, pe, hidden, minibatch)
 
     vocab = Vocab("../data/features.data")
 
     network = Network(vocab, net_properties)
+
+    network.train("../data/features.data", epochs)
