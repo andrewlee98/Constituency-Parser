4cefbee2c7fb3b598a631f957530b31f0a423601
andrewwlee98 <andrew.warren.lee@gmail.com>
2019-03-01 23:09:27 -0500

    fixed bug by using combined vocab for both training and test sets

--- src-allen/embed_net.py
+++ src-allen/embed_net.py
@@ -9,6 +9,8 @@ from torch import nn
 from torch.optim import Adam
 from torch.autograd import Variable
 from datetime import datetime
+from matplotlib import pyplot as plt
+import os
 startTime = datetime.now()
 
 device = torch.device("cuda:0")
@@ -16,10 +18,10 @@ device = torch.device("cuda:0")
 
 # class for loading training/testing datasets
 class CPDataset(Dataset):
-    def __init__(self, datapath):
+    def __init__(self, fv_list, vocab):
         # load training, validation data, and vocab
-        self.data = np.array(pickle.load(open(datapath, 'rb')))
-        self.vocab = Vocab(datapath)
+        self.data = np.array(fv_list)
+        self.vocab = vocab
         self.length = self.data.shape[0]
 
         # turn feature vectors from words/labels into numbers
@@ -59,7 +61,6 @@ class Net(nn.Module):
         self.fc2 = nn.Linear(hidden_size, num_classes) # 2nd Full-Connected Layer: 500 (hidden node) -> 89 (output class)
 
     def forward(self, x):  # Forward pass: stacking each layer together
-
         x = x.to(self.device)
         x = x.cuda()
         embeds = self.embeddings(x).view(x.shape[0],-1)
@@ -68,21 +69,39 @@ class Net(nn.Module):
         out = self.fc2(out)
         return out
 
+def test(net, data_loader):
+    total,correct = 0, 0
+    for fvs, labels in data_loader:
+        fvs = Variable(fvs.cuda())
+        outputs = net(fvs.cuda())
+        _, predicted = torch.max(outputs.data, 1)  # Choose the best class from the output: The class with the best score
+        total += labels.size(0)                    # Increment the total count
+        correct += (predicted.cuda() == labels.cuda()).sum()     # Increment the correct count
+    return (float(correct) / float(total))
+
 
 if __name__ == '__main__':
     input_size = 27        # 27 features
-    hidden_size = 500      # The number of nodes at the hidden layer
+    hidden_size = 200      # The number of nodes at the hidden layer
     num_classes = 89       # The number of output classes.
     num_epochs = 5
     batch_size = 100
-    learning_rate = 0.001
+    learning_rate = 0.0001
     vocab_size = 100000    # keep track of some word's embeddings
     embedding_dim = 100   # word embedding size
 
 
     # load data into dataset objects
-    train_data = CPDataset('../data/mini/features/train.data')
-    test_data = CPDataset('../data/mini/features/validation.data')
+    all_data = []
+    for file in os.listdir('../data/allen/features/'):
+        if file[0:2] not in {'00','01','02','03','04'}: continue
+        all_data.extend(pickle.load(open('../data/allen/features/' + file, 'rb')))
+    print('Total # feature vectors: ', len(all_data))
+
+    vocab = Vocab(all_data)
+    train_test_split = len(all_data)*9//10
+    train_data = CPDataset(all_data[train_test_split:], vocab)
+    test_data = CPDataset(all_data[:train_test_split], vocab)
 
     # convert to dataloaders
     train_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
@@ -95,6 +114,7 @@ if __name__ == '__main__':
     optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
 
     # training
+    losses, validations, trains = [], [], []
     for epoch in range(num_epochs):
         for i, (fvs, labels) in enumerate(train_loader):   # Load a batch of images with its (index, data, class)
             fvs = Variable(fvs)
@@ -109,18 +129,40 @@ if __name__ == '__main__':
             if (i+1) % 100 == 0:  # Logging
                 print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
                          %(epoch+1, num_epochs, i+1, len(train_data)//batch_size, loss.data))
+                losses.append((epoch, loss.data.item())) # plot loss over time
 
-    # testing
-    total,correct = 0, 0
-    for fvs, labels in test_loader:
-        fvs = Variable(fvs)
-        outputs = net(fvs.cuda())
-        _, predicted = torch.max(outputs.data, 1)  # Choose the best class from the output: The class with the best score
-        total += labels.size(0)                    # Increment the total count
-        correct += (predicted.cuda() == labels.cuda()).sum()     # Increment the correct count
+        # test validation accuracy at the end of every epoch
+        val_error = test(net, test_loader)
+        validations.append((epoch, val_error))
+        train_error = test(net, train_loader)
+        trains.append((epoch, train_error))
+        print(val_error, train_error)
 
-    print('Accuracy: %f %%' % (100 * correct / total))
+    print('Test Accuracy: %f' % (test(net, test_loader)))
+    print('Train Accuracy: %f' % (test(net, train_loader)))
 
     # save the net
     # torch.save(net.state_dict(), ‘fnn_model.pkl’)
     print(datetime.now() - startTime)
+
+
+    # plot stuff
+    plt.title("Loss over time")
+    plt.xlabel("Minibatch")
+    plt.ylabel("Loss")
+    plt.plot(*zip(*losses))
+    plt.savefig('loss.png')
+
+
+
+    plt.figure()
+    plt.title("Training/Validation Accuracy")
+    plt.xlabel("Minibatch")
+    plt.ylabel("Accuracy")
+    x1, y1 = zip(*validations)
+    plt.plot(x1, y1, 'r')
+
+    x2, y2 = zip(*trains)
+    plt.ylim((0, 1))
+    plt.plot(x2, y2, 'g')
+    plt.savefig('accuracy.png')
